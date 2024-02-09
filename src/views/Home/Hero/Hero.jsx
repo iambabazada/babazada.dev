3c4384c2fd623b228978627c6b5c4f733472cb80
iambabazada <iambabazada@gmail.com>
2024-02-10 00:55:13 +0400

    bg video

--- src/views/Home/Hero/Hero.jsx
+++ src/views/Home/Hero/Hero.jsx
@@ -3,17 +3,23 @@ import styles from './Hero.module.css'
 import Button from '../../../components/Button'
 import Person from '@/assets/images/iambabazada.jpg'
 import cvDownload from '@/assets/cv.pdf'
+import HeroVideo from '/hero.mp4'
 
 const Hero = () => {
     return (
         <section id="hero_section" className={`${styles.hero_section}  `}>
+            <div className={styles.bg_video}>
+                <div className={styles.overlay}>
+                </div>
+                <video src={HeroVideo} autoPlay={true} loop={true} controls={true} className={styles.video}></video>
+            </div>
             <div className={`${styles.hero_section_content_box} `}>
                 <div className={styles.hero_section_content}>
                     <p>Hey I'm Shahriyar</p>
                     <h1 className='text-5xl font-semibold'>
                         Front end <br /> Developer
                     </h1>
-                    <p className='text-secondary'>Transforming visions into seamless digital solutions.</p>
+                    <p className='text-accent'>Transforming visions into seamless digital solutions.</p>
                     <div>
                         <a
                             href={cvDownload}
@@ -31,7 +37,7 @@ const Hero = () => {
                     <img src={Person} alt="" />
                 </div>
             </div>
-        </section>
+        </section >
     )
 }
 
